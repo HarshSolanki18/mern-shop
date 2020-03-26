a858cd7df1d94ff506aec9203939c071f1397856
harsh solanki <hsolanki1884@gmail.com>
2020-03-26 16:17:31 +0530

    cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const items=require('./routes/api/items');
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
   });
 
