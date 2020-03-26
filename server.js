cc917aac8ac1f674f130b0ba4850cb4e79c0c940
harsh solanki <hsolanki1884@gmail.com>
2020-03-26 14:21:54 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express"),
      bodyParser=require("body-parser"),
-     mongoose=require("mongoose");
+     mongoose=require("mongoose"),
+     path=require("path");
 
 const app=express();
 const items=require('./routes/api/items');
@@ -15,14 +16,23 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 //DB config
 const db=require('./config/keys').mongoURI;
-//connect to mongodb
-mongoose.connect('mongodb://localhost:27017/itemdb').then(()=>{
+//connect to mongodb  mongodb://localhost:27017/itemdb
+mongoose.connect(db).then(()=>{
     console.log("Database Connected ...")}).catch((err)=>{
         console.log(err);
     });
 
 app.use('/api/items',items);    
 
+//serve static assets if in production
+if(process.env.NODE_ENV=='production'){
+    ///set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+
+}
 const port=process.env.port||5000;
 //connect to server
 app.listen(port,()=>console.log(`server connected on port  ${port}`));
